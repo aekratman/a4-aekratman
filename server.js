6505d7fa5d807040b8b68c9bf64b7a4b476b3d83
abbey <aekratman@gmail.com>
2024-09-30 01:43:34 -0400

    blathrhghrhggh

--- server.js
+++ server.js
@@ -5,6 +5,13 @@ const PORT = 3000;
 // Middleware to parse JSON requests
 app.use(express.json());
 
+app.use(express.static(path.join(__dirname, 'dist'))); // Ensure this points to your build output
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+});
+
+
 
 // Start the server
 app.listen(PORT, () => {
